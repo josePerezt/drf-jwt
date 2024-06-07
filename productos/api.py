b673ae03a63ac6a3802a75a8f740d94d4599870a
joseDev <josemptj@gmail.com>
2024-06-07 12:49:13 -0300

    add jwt

--- productos/api.py
+++ productos/api.py
@@ -6,4 +6,4 @@ from .models import Product
 class ProductViewSet(viewsets.ModelViewSet):
   queryset = Product.objects.all()
   serializer_class = ProducSerializer
-  permission_classes = [permissions.AllowAny]
+  permission_classes = [permissions.IsAuthenticated]
