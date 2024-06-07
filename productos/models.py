d416eaafcb07784cc787b59c9494fade390f7fdc
joseDev <josemptj@gmail.com>
2024-06-07 11:37:39 -0300

    create app productos

---
+++ productos/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+  title = models.CharField(max_length=200)
+  description = models.TextField()
+  marca = models.CharField(max_length=50)
+  quantity_min = models.IntegerField()
+  quantity_max = models.IntegerField()
+  price = models.DecimalField(max_digits=2,decimal_places=2)
